57564a82f6d0ea6a37ce99381e766d98f252a1b8
Nils Sommer <mail@nilssommer.de>
2016-12-10 13:42:08 +0100

    Add SCSS structure and grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,15 @@ module.exports = function (grunt) {
     pkg: grunt.file.readJSON('package.json'),
 
     clean: {
-      js: 'dist/'
+      js:  [
+        'dist/*.js',
+        'dist/*.js.map',
+        'dist/*.coffee'
+      ],
+      css: [
+        'dist/*.css',
+        'dist/*.css.map'
+      ]
     },
 
     coffee: {
@@ -16,7 +24,10 @@ module.exports = function (grunt) {
           sourceMap: true
         },
         files: {
-          'dist/datepicker.js': 'coffee/**/*.coffee'
+          'dist/datepicker.js': [
+            'coffee/views/*.coffee',
+            'coffee/datepicker.coffee'
+          ]
         }
       }
     },
@@ -31,11 +42,41 @@ module.exports = function (grunt) {
         }
       }
     },
+    
+    sass: {
+      bundle: {
+        options: {
+          style: 'expanded'
+        },
+        files: {
+          'dist/datepicker.css': 'scss/datepicker.scss'
+        }
+      }
+    },
+    
+    cssmin: {
+      options: {
+        sourceMap: true
+      },
+      bundle: {
+        files: [{
+          expand: true,
+          cwd: 'dist',
+          src: ['*.css', '!*.min.css'],
+          dest: 'dist',
+          ext: '.min.css'
+        }]
+      }
+    },
 
     watch: {
       coffee: {
         files: ['coffee/**/*.coffee'],
         tasks: ['build-js']
+      },
+      sass: {
+        files: ['scss/**/*.scss'],
+        tasks: ['build-css']
       }
     },
 
@@ -52,10 +93,14 @@ module.exports = function (grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-connect');
 
+  grunt.registerTask('build-css', ['clean:css', 'sass', 'cssmin']);
   grunt.registerTask('build-js', ['clean:js', 'coffee', 'uglify']);
-  grunt.registerTask('build', ['build-js']);
+  grunt.registerTask('build', ['build-css', 'build-js']);
 }
